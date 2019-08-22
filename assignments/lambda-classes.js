2331b0502bf99ecb2f76cc7751a5901a5a1365ec
LenWinkler <lenwinkler86@gmail.com>
2019-08-22 16:20:42 -0400

    finished creating classes for lambda-classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,73 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(attrs){
+        this.name = attrs.name,
+        this.age = attrs.age,
+        this.location = attrs.location
+    }
+
+    speak(){
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
+    }
+}
+
+class Instructor extends Person{
+    constructor(attrs){
+        super(attrs),
+        this.specialty = attrs.specialty,
+        this.favLanguage = attrs.favLanguage,
+        this.catchPhrase = attrs.catchPhrase
+    }
+
+    demo(subject){
+        console.log(`Today we are learning about ${subject}`)
+    }
+
+    grade(subject){
+        console.log(`${student.name} receives a perfect score on ${subject}`)
+    }
+
+}
+
+class Student extends Person {
+    constructor(attrs){
+        super(attrs),
+        this.previousBackground = attrs.previousBackground,
+        this.className = attrs.className,
+        this.favSubjects = attrs.favSubjects
+    }
+
+    listsSubjects(){
+        console.log(this.favSubjects.values());
+    }
+
+    PRAssignment(subject){
+        console.log(`${student.name} has submitted a PR for ${subject}`)
+    }
+
+    sprintChallenge(subject){
+        console.log(`${student.name} has begun sprint challenge on ${subject}`)
+    }
+
+}
+
+class ProjectManager extends Instructor {
+    constructor(attrs){
+        super(attrs),
+        this.gradClassName = attrs.gradClassName,
+        this.favInstructor = attrs.favInstructor
+    }
+
+    standUp(channel){
+        console.log(`${this.name} announces to ${channel} @channel standy times!`)
+    }
+
+    debugsCode(name, subject){
+        console.log(`${name} debugs ${student.name}'s code on ${subject}`)
+    }
+
+}
+
+/*--------------------Test Objects-----------------*/
+
